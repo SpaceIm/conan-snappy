1a5895bf3788307a33470cc81d89ee5afce0ba72
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2020-03-06 11:31:04 +0100

    Initial commit

---
+++ conanfile.py
@@ -0,0 +1,62 @@
+import os
+
+from conans import ConanFile, CMake, tools
+
+class SnappyConan(ConanFile):
+    name = "snappy"
+    description = "A fast compressor/decompressor"
+    topics = ("conan", "snappy", "google", "compressor", "decompressor")
+    url = "https://github.com/conan-io/conan-center-index"
+    homepage = "https://github.com/google/snappy"
+    license = "BSD-3-Clause"
+    exports_sources = ["CMakeLists.txt"]
+    generators = "cmake"
+    settings = "os", "arch", "compiler", "build_type"
+    options = {"shared": [True, False], "fPIC": [True, False]}
+    default_options = {"shared": False, "fPIC": True}
+
+    _cmake = None
+
+    @property
+    def _source_subfolder(self):
+        return "source_subfolder"
+
+    @property
+    def _build_subfolder(self):
+        return "build_subfolder"
+
+    def config_options(self):
+        if self.settings.os == 'Windows':
+            del self.options.fPIC
+
+    def configure(self):
+        if self.options.shared:
+            del self.options.fPIC
+
+    def source(self):
+        tools.get(**self.conan_data["sources"][self.version])
+        extracted_dir = "{}-{}".format(self.name, self.version)
+        os.rename(extracted_dir, self._source_subfolder)
+
+    def _configure_cmake(self):
+        if self._cmake:
+            return self._cmake
+        self._cmake = CMake(self)
+        self._cmake.definitions["SNAPPY_BUILD_TESTS"] = False
+        self._cmake.configure(build_folder=self._build_subfolder)
+        return self._cmake
+
+    def build(self):
+        cmake = self._configure_cmake()
+        cmake.build()
+
+    def package(self):
+        self.copy(pattern="COPYING", dst="licenses", src=self._source_subfolder)
+        cmake = self._configure_cmake()
+        cmake.install()
+        tools.rmdir(os.path.join(self.package_folder, "lib", "cmake"))
+
+    def package_info(self):
+        self.cpp_info.names["cmake_find_package"] = "Snappy"
+        self.cpp_info.names["cmake_find_package_multi"] = "Snappy"
+        self.cpp_info.libs = tools.collect_libs(self)
