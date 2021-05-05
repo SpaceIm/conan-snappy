15c217d3e1b76555abc14ae9ae556e55323682f0
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2021-05-06 01:02:26 +0200

    consistent quotes

--- conanfile.py
+++ conanfile.py
@@ -28,7 +28,7 @@ class SnappyConan(ConanFile):
         return "build_subfolder"
 
     def config_options(self):
-        if self.settings.os == 'Windows':
+        if self.settings.os == "Windows":
             del self.options.fPIC
 
     def configure(self):
